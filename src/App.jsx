27b908b17930eb42adc7ab88c8eb1b375e8da6d7
unknown <yonideserbaix@gmail.com>
2024-06-02 16:31:42 +0200

    feat(app): import bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="bg-[url('../../public/assets/background.png')] w-full h-[100vh] bg-center">
+      <div>
+        <h1 className="text-3xl text-white font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </div>
+  );
 }
