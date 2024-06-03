0eb5e1ad421eaceeaaddddaf54f2935810eb4551
unknown <yonideserbaix@gmail.com>
2024-06-03 14:00:25 +0200

    style(navbar): color text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
 export default function Hero() {
   return (
     <div className="mt-28 text-center">
-      <p className="font-light text-white">
+      <p className="font-light text-[#F2F4F8]">
         We just raised $20M in Series B. Learn more
       </p>
-      <h1 className="text-7xl font-medium">
+      <h1 className="text-7xl font-medium text-[#ECECEC]">
         Modern analytics <br /> <span>for the modern world</span>
       </h1>
-      <p className="text-lg">
+      <p className="text-lg font-light text-[#ECECECA6] opacity-95">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
         Suspendisse varius enim in eros elementum tristique.
       </p>
