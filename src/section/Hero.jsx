050ba8e0afa7e427a787b9bf430ca820c050b7dd
unknown <yonideserbaix@gmail.com>
2024-06-03 15:08:10 +0200

    style(hero): h1 gradient

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,5 +1,4 @@
 import Button from "../components/button";
-
 export default function Hero() {
   return (
     <div className="mt-28 text-center">
@@ -8,16 +7,19 @@ export default function Hero() {
         className="hover:none mx-auto flex w-[417px] cursor-auto justify-center border-opacity-30 py-2.5"
         invert
       ></Button>
-      <h1 className="text-7xl font-medium text-[#ECECEC]">
-        Modern analytics <br /> <span>for the modern world</span>
+      <h1 className="m-7 text-7xl font-medium text-[#ECECEC]">
+        Modern analytics <br />
+        <span className="bg-gradient-to-r from-[#ecececbe] to-[#ececec5c] bg-clip-text text-transparent">
+          for the modern world
+        </span>
       </h1>
       <p className="text-lg font-light text-[#ECECECA6] opacity-95">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
         Suspendisse varius enim in eros elementum tristique.
       </p>
-      <div className="m-auto flex max-w-xl items-center justify-center gap-4">
-        <Button text="Download the app" className="py-3" />
-        <Button text="Talk to an expert" invert className="py-3 font-light" />
+      <div className="m-auto mt-8 flex max-w-xl items-center justify-center gap-4">
+        <Button text="Download the app" className="py-2.5" />
+        <Button text="Talk to an expert" invert className="py-2.5 font-light" />
       </div>
     </div>
   );
