e20ae3a11ef6ad5883c5df2bd02c527e99312f6b
unknown <yonideserbaix@gmail.com>
2024-06-03 13:34:43 +0200

    style(navbar): add animation on hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <div>
       <div className="flex justify-between items-center bg-[#0B081C] bg-opacity-50 px-10 text-white py-2.5 m-8 rounded-3xl">
-        <div className="flex gap-2 text-xl">
+        <div className="flex gap-2 text-xl cursor-pointer">
           <img
             src="../assets/Valtflow.svg"
             alt="Vaultflow logo"
@@ -11,11 +11,17 @@ export default function Navbar() {
           <p className="font-bold">Vaultflow</p>
         </div>
         <ul className="flex-row flex gap-16 font-light">
-          <li>Features</li>
-          <li>Pricing</li>
-          <li>About us</li>
+          <li className=" hover:text-gray-300 transition-all cursor-pointer">
+            Features
+          </li>
+          <li className=" hover:text-gray-300 transition-all cursor-pointer">
+            Pricing
+          </li>
+          <li className=" hover:text-gray-300 transition-all cursor-pointer">
+            About us
+          </li>
         </ul>
-        <div className="text-black bg-white py-2 my-1 px-5 rounded-3xl">
+        <div className="text-black bg-white py-2 my-1 px-5 rounded-3xl cursor-pointer transition-all hover:bg-gray-300">
           Downloap the app
         </div>
       </div>
