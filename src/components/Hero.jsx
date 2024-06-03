a5a1905570f5e1e34e79da35dab9670d7ca8e5e8
unknown <yonideserbaix@gmail.com>
2024-06-03 13:53:13 +0200

    feat(hero): add h1 and paragraph

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
 export default function Hero() {
   return (
-    <div className="mt-28">
-      <p className="text-center font-light text-white">
+    <div className="mt-28 text-center">
+      <p className="font-light text-white">
         We just raised $20M in Series B. Learn more
+      </p>
+      <h1 className="text-7xl font-medium">
+        Modern analytics <br /> <span>for the modern world</span>
+      </h1>
+      <p className="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+        Suspendisse varius enim in eros elementum tristique.
       </p>
     </div>
   );
