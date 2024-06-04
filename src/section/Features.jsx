30eb42882f1bb74ee8970e671bfe32c9ebc9aafe
unknown <yonideserbaix@gmail.com>
2024-06-04 22:04:17 +0200

    style(features): size & position of h1

--- src/section/Features.jsx
+++ src/section/Features.jsx
@@ -1,7 +1,7 @@
 export default function Features() {
   return (
-    <div className="m-28">
-      <h1 className="text-center text-white">
+    <div className="m-20">
+      <h1 className="mx-auto max-w-96 text-center text-5xl leading-tight text-white">
         Features that work for your future.
       </h1>
     </div>
