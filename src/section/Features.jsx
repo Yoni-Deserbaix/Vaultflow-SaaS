0ea077c688f26eb77db9e34ba345d8e26eb91b07
unknown <yonideserbaix@gmail.com>
2024-06-04 22:16:51 +0200

    feat(features): add blur effect

--- src/section/Features.jsx
+++ src/section/Features.jsx
@@ -1,7 +1,12 @@
 export default function Features() {
   return (
-    <div className="m-20">
-      <h1 className="mx-auto max-w-96 text-center text-5xl leading-tight text-white">
+    <div className="relative m-20 text-center">
+      <img
+        src="./assets/blur.png"
+        alt=""
+        className="absolute -top-16 left-0 right-0 m-auto w-[470px] transform"
+      />
+      <h1 className="relative z-10 mx-auto max-w-96 text-5xl leading-tight text-white">
         Features that work for your future.
       </h1>
     </div>
