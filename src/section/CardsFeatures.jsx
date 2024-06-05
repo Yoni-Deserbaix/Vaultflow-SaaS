75761a3386a09fcff8d9dec10c6befdc48be6d49
unknown <yonideserbaix@gmail.com>
2024-06-05 15:51:39 +0200

    style(cardFeatures): responsive and flexbox

--- src/section/CardsFeatures.jsx
+++ src/section/CardsFeatures.jsx
@@ -7,7 +7,6 @@ export default function CardsFeatures() {
         "Our Analytics Dashboard provides a clear and intuitive interface for you to easily analyze your data. From customizable graphs to real-time data updates, our dashboard offers everything you need to gain valuable insights.",
       viewLink: "View dashboard",
     },
-
     {
       src: "./assets/icon/icon02.svg",
       title: "Digital Credit Tokens",
@@ -25,31 +24,43 @@ export default function CardsFeatures() {
     },
   ];
 
-  const slicedCards = cards.slice(0, 2);
-
   return (
-    <div className="mx-56">
-      <div className="grid grid-cols-2 gap-6">
-        {slicedCards.map((card, index) => (
-          <div key={index}>
-            <div className="h-[372px] rounded-lg bg-gray-200">
-              <div className="p-16">
-                <img src={card.src} alt="icon" className="" />
-                <h1>{card.title}</h1>
-                <p>{card.caption}</p>
-                <span>{card.viewLink}</span>
+    <div className="mx-auto max-w-screen-xl p-4">
+      <div className="grid grid-cols-2 gap-6 max-[1024px]:grid-cols-1">
+        {cards.slice(0, 2).map((card, index) => (
+          <div key={index} className="flex">
+            <div className="flex h-full w-full flex-col justify-between rounded-lg bg-indigo-900 p-14">
+              <div>
+                <img src={card.src} alt="icon" />
+                <h1 className="pb-3 pt-4 text-[32px] text-[#ECECEC]">
+                  {card.title}
+                </h1>
+                <p className="text-[#ECECEC]">{card.caption}</p>
               </div>
+              <p className="pt-8 text-white underline">{card.viewLink}</p>
             </div>
           </div>
         ))}
       </div>
-      <div className="mt-20 h-[372px] w-full rounded-lg bg-gray-200">
-        <div className="p-16">
-          <img src={cards[2].src} alt="icon" />
-          <h1>{cards[2].title}</h1>
-          <p>{cards[2].caption}</p>
-          <span>{cards[2].viewLink}</span>
-          <img src={cards[2].imageCode} alt="DigitalToken.js code" />
+      <div className="mt-20 flex h-full w-full flex-row justify-between rounded-lg bg-indigo-900 p-14 max-lg:mt-6 max-md:flex-col">
+        <div className="flex w-1/2 pr-4 max-lg:w-1/2 max-md:w-full">
+          <div>
+            <img src={cards[2].src} alt="icon" className="mb-4 h-12 w-12" />
+            <h1 className="pb-3 pr-8 pt-4 text-[32px] text-[#ECECEC]">
+              {cards[2].title}
+            </h1>
+            <p className="w-full max-w-md text-[#ECECEC] max-sm:w-full">
+              {cards[2].caption}
+            </p>
+            <p className="pt-8 text-white underline">{cards[2].viewLink}</p>
+          </div>
+        </div>
+        <div className="flex w-1/2 items-center justify-center max-lg:mt-8 max-md:w-full">
+          <img
+            src={cards[2].imageCode}
+            alt="DigitalToken.js code"
+            className=""
+          />
         </div>
       </div>
     </div>
