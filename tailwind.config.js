9747d46c4c0c10ba83e2473db7bc6351471f7293
unknown <yonideserbaix@gmail.com>
2024-06-05 17:36:59 +0200

    fix(tailwind.config): remove useless data

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      boxShadow: {
-        "custom-analytics": "0px 0px 36.45px 0px #591DDDBF",
-        "custom-tokens": "0px 0px 36.45px 0px #9525C9BF",
-        "custom-collab": "0px 0px 36.45px 0px #C925ABBF",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
