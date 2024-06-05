2611e3cada92602034d2223407cc7a101936bc52
unknown <yonideserbaix@gmail.com>
2024-06-05 16:12:35 +0200

    style(cardsFeatures): background & border gradient

--- src/section/CardsFeatures.jsx
+++ src/section/CardsFeatures.jsx
@@ -24,12 +24,23 @@ export default function CardsFeatures() {
     },
   ];
 
+  const cardStyle = {
+    boxShadow: `0px 0.5px 0px 0px #FFFFFF80 inset,
+                0px -2px 40px 0px #BB9BFF26,
+                0px -2px 10px 0px #E9DFFF4D`,
+    background:
+      "linear-gradient(180deg, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 0) 100%)",
+  };
+
   return (
     <div className="mx-auto max-w-screen-xl p-4">
       <div className="grid grid-cols-2 gap-6 max-[1024px]:grid-cols-1">
         {cards.slice(0, 2).map((card, index) => (
           <div key={index} className="flex">
-            <div className="flex h-full w-full flex-col justify-between rounded-lg bg-indigo-900 p-14">
+            <div
+              className="flex h-full w-full flex-col justify-between rounded-3xl p-14"
+              style={cardStyle}
+            >
               <div>
                 <img src={card.src} alt="icon" />
                 <h1 className="pb-3 pt-4 text-[32px] text-[#ECECEC]">
@@ -37,12 +48,17 @@ export default function CardsFeatures() {
                 </h1>
                 <p className="text-[#ECECEC]">{card.caption}</p>
               </div>
-              <p className="pt-8 text-white underline">{card.viewLink}</p>
+              <p className="mt-8 cursor-pointer text-white underline">
+                {card.viewLink}
+              </p>
             </div>
           </div>
         ))}
       </div>
-      <div className="mt-20 flex h-full w-full flex-row justify-between rounded-lg bg-indigo-900 p-14 max-lg:mt-6 max-md:flex-col">
+      <div
+        className="mt-20 flex h-full w-full flex-row justify-between rounded-3xl p-14 max-lg:mt-6 max-md:flex-col"
+        style={cardStyle}
+      >
         <div className="flex w-1/2 pr-4 max-lg:w-1/2 max-md:w-full">
           <div>
             <img src={cards[2].src} alt="icon" className="mb-4 h-12 w-12" />
@@ -52,14 +68,17 @@ export default function CardsFeatures() {
             <p className="w-full max-w-md text-[#ECECEC] max-sm:w-full">
               {cards[2].caption}
             </p>
-            <p className="pt-8 text-white underline">{cards[2].viewLink}</p>
+            <p className="mt-8 cursor-pointer text-white underline">
+              {cards[2].viewLink}
+            </p>
           </div>
         </div>
         <div className="flex w-1/2 items-center justify-center max-lg:mt-8 max-md:w-full">
           <img
             src={cards[2].imageCode}
             alt="DigitalToken.js code"
-            className=""
+            style={cardStyle}
+            className="rounded-3xl"
           />
         </div>
       </div>
