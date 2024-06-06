35944f31175080c85900304e7a5afae84f54ad7a
unknown <yonideserbaix@gmail.com>
2024-06-06 19:02:27 +0200

    feat(component): create cardGradient component

--- src/section/CardsFeatures.jsx
+++ src/section/CardsFeatures.jsx
@@ -1,15 +1,8 @@
+import { cardStyle } from "../components/cardGradient";
 import cardsData from "../data";
 
 export default function CardsFeatures() {
   const { cardsData: card } = cardsData;
-
-  const cardStyle = {
-    boxShadow: `0px 0.5px 0px 0px #FFFFFF80 inset,
-                0px -2px 40px 0px #BB9BFF26,
-                0px -2px 10px 0px #E9DFFF4D`,
-    background:
-      "linear-gradient(180deg, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 0) 100%)",
-  };
 
   return (
     <div className="mx-auto max-w-screen-xl p-4">
