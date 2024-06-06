1d86b1c8e6824a9634a56884614c75610c83ebf6
unknown <yonideserbaix@gmail.com>
2024-06-06 19:22:24 +0200

    feat(heroNoImage): add html elements

--- src/section/HeroNoImage.jsx
+++ src/section/HeroNoImage.jsx
@@ -1,3 +1,31 @@
+import Button from "../components/button";
+import { cardStyle } from "../components/cardGradient";
 export default function HeroNoImage() {
-  return <div>HeroNoImage</div>;
+  return (
+    <div>
+      <div className="mx-auto max-w-screen-xl p-4">
+        <div
+          className="mt-8 flex flex-col justify-center rounded-3xl p-14 text-center text-[#ECECEC]"
+          style={cardStyle}
+        >
+          <div className="mx-auto max-w-3xl">
+            <h1 className="text-5xl font-bold">
+              Our powerful analytics provides invaluable insights.
+            </h1>
+            <p>
+              Unlock the power of data with our cutting-edge analytics product.
+              Get instant insights with our user-friendly Analytics Dashboard,
+              and take advantage of our innovative digital credit tokens to
+              reward your customers and incentivize engagement.
+            </p>
+            <Button
+              text="Download the app"
+              invert
+              className="mx-auto w-48 py-2.5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
