060a8d9e49ed05301ef140728549710f978e5712
unknown <yonideserbaix@gmail.com>
2024-06-08 12:55:37 +0200

    style(footer): responsive grid

--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -2,72 +2,70 @@ import data from "../data";
 
 export default function Footer() {
   return (
-    <div className="mx-auto mt-28 max-w-screen-xl p-4 pb-16 text-white">
-      <div>
-        {data.footer.map((item, index) => (
-          <div
-            key={index}
-            className="grid h-[290px] w-[924px] grid-cols-2 justify-between gap-6"
-          >
-            <div>
-              <h2 className="pb-3 text-2xl font-medium">Contact</h2>
-              <p className="text-lg font-extralight leading-6">
-                Work inquiries:{" "}
-                <a href={`mailto:${item.contact.work_inquiries}`}>
-                  {item.contact.work_inquiries}
-                </a>
-              </p>
-              <p className="text-lg font-extralight leading-6">
-                PR and speaking:{" "}
-                <a href={`mailto:${item.contact.pr_and_speaking}`}>
-                  {item.contact.pr_and_speaking}
-                </a>
-              </p>
-              <p className="text-lg font-extralight leading-6">
-                New business:{" "}
-                <a href={`mailto:${item.contact.new_business}`}>
-                  {item.contact.new_business}
-                </a>
-              </p>
-            </div>
-            <div>
-              <h2 className="pb-3 text-2xl font-medium">Address</h2>
-              <p className="text-lg font-extralight leading-6">
-                {item.address.street}
-              </p>
-              <p className="text-lg font-extralight leading-6">
-                {item.address.city}
-              </p>
-            </div>
-            <div>
-              <h2 className="pb-3 text-2xl font-medium">Careers</h2>
-              <p className="text-lg font-extralight leading-6">
-                <a href={`mailto:${item.careers}`}>{item.careers}</a>
-              </p>
-            </div>
-            <div>
-              <h2 className="pb-3 text-2xl font-medium">Social</h2>
-              <ul>
-                {item.social.map((platform, index) => (
-                  <li
-                    className="cursor-pointer text-lg font-extralight"
-                    key={index}
-                  >
-                    {platform}
-                  </li>
-                ))}
-              </ul>
-            </div>
+    <div className="mx-auto mt-28 max-w-screen-xl p-8 pb-16 text-white max-md:pb-52">
+      {data.footer.map((item, index) => (
+        <div
+          key={index}
+          className="grid h-auto w-full grid-cols-2 gap-6 max-md:grid-cols-1"
+        >
+          <div>
+            <h2 className="pb-3 text-2xl font-medium">Contact</h2>
+            <p className="text-lg font-extralight leading-6">
+              Work inquiries:{" "}
+              <a href={`mailto:${item.contact.work_inquiries}`}>
+                {item.contact.work_inquiries}
+              </a>
+            </p>
+            <p className="text-lg font-extralight leading-6">
+              PR and speaking:{" "}
+              <a href={`mailto:${item.contact.pr_and_speaking}`}>
+                {item.contact.pr_and_speaking}
+              </a>
+            </p>
+            <p className="text-lg font-extralight leading-6">
+              New business:{" "}
+              <a href={`mailto:${item.contact.new_business}`}>
+                {item.contact.new_business}
+              </a>
+            </p>
           </div>
-        ))}
-      </div>
-      <div className="flex justify-between">
+          <div>
+            <h2 className="pb-3 text-2xl font-medium">Address</h2>
+            <p className="text-lg font-extralight leading-6">
+              {item.address.street}
+            </p>
+            <p className="text-lg font-extralight leading-6">
+              {item.address.city}
+            </p>
+          </div>
+          <div>
+            <h2 className="pb-3 text-2xl font-medium">Careers</h2>
+            <p className="text-lg font-extralight leading-6">
+              <a href={`mailto:${item.careers}`}>{item.careers}</a>
+            </p>
+          </div>
+          <div>
+            <h2 className="pb-3 text-2xl font-medium">Social</h2>
+            <ul>
+              {item.social.map((platform, index) => (
+                <li
+                  className="cursor-pointer text-lg font-extralight"
+                  key={index}
+                >
+                  {platform}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ))}
+      <div className="mt-6 flex justify-between">
         <p className="font-extralight leading-6 text-[#939393]">
           &copy; 2023 Vaultflow. All Rights Reserved.
         </p>
         <div className="flex gap-3">
           <img
-            src="../assets/Valtflow.svg"
+            src="../assets/Vaultflow.svg"
             alt="Vaultflow logo"
             className="h-6 w-6"
           />
