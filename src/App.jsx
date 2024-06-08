3379dfd2e66a119fe83ed46d733ab540f2644eb8
unknown <yonideserbaix@gmail.com>
2024-06-08 18:21:45 +0200

    fix(hero): bg image path

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import Partner from "./section/Partner";
 import Features from "./section/Features";
 import Footer from "./section/Footer";
 import Hero from "./section/Hero";
 import HeroImage from "./section/HeroImage";
 import HeroNoImage from "./section/HeroNoImage";
 import Navbar from "./section/Navbar";
+import Partner from "./section/Partner";
 
 export default function App() {
   return (
     <div>
       <div className="relative">
-        <div className="absolute w-full bg-[url('./assets/background.png')] bg-top bg-repeat-x">
+        <div className="absolute w-full bg-[url('../public/assets/background.png')] bg-top bg-repeat-x">
           <Navbar />
           <div className="mx-auto max-w-4xl">
             <Hero />
