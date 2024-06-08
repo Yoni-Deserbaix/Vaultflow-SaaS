ffd17cab67a1648dacb67a681e5b719aecd8da18
unknown <yonideserbaix@gmail.com>
2024-06-08 16:28:10 +0200

    style(brands): responsive

--- src/section/Brand.jsx
+++ src/section/Brand.jsx
@@ -11,13 +11,17 @@ export default function Brand() {
 
   return (
     <div>
-      <p className="pt-36 text-center font-light text-white opacity-80">
+      <p className="pt-36 text-center font-light text-white opacity-80 max-sm:mx-4">
         Trusted by teams at over 1,000 of the world’s leading organizations
       </p>
-      <div className="mt-8 flex items-center justify-center gap-3">
+      <div className="mt-8 flex items-center justify-center gap-3 max-md:grid max-md:grid-cols-3 max-md:flex-col">
         {brandsLogo.map((logo, index) => (
-          <div key={index} className="m-4">
-            <img src={logo.src} alt={logo.name} className="w-full h-full" />
+          <div key={index} className="p-4">
+            <img
+              src={logo.src}
+              alt={logo.name}
+              className="max-h-full max-w-full"
+            />
           </div>
         ))}
       </div>
