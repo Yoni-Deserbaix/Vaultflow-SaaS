ea7c4657f6b0bf4b70ede108d0d645374f29627b
unknown <yonideserbaix@gmail.com>
2024-06-09 14:55:30 +0200

    style(heroImage): animation with framer motion

--- src/section/HeroImage.jsx
+++ src/section/HeroImage.jsx
@@ -1,11 +1,11 @@
+import FadeUp from "../components/FadeUp";
+
 export default function HeroImage() {
   return (
     <div>
-      <img
-        src="./assets/hero_image.svg"
-        alt="hero image"
-        className="animate-fade-up"
-      />
+      <FadeUp delay={0.8}>
+        <img src="./assets/hero_image.svg" alt="hero image" />
+      </FadeUp>
     </div>
   );
 }
