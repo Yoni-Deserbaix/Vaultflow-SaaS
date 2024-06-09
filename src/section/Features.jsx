16cf479f087fd18f2c419b205913a8443e7ff453
unknown <yonideserbaix@gmail.com>
2024-06-09 15:14:31 +0200

    style(features): animation with framer motion

--- src/section/Features.jsx
+++ src/section/Features.jsx
@@ -1,3 +1,4 @@
+import FadeOnScroll from "../components/FadeOnScroll";
 import CardsFeatures from "./CardsFeatures";
 
 export default function Features() {
@@ -9,13 +10,17 @@ export default function Features() {
           alt=""
           className="absolute -top-16 left-0 right-0 m-auto w-[470px] transform"
         />
-        <h1 className="relative z-10 mx-auto max-w-96 text-5xl leading-tight max-md:text-4xl">
-          Features that work for your future.
-        </h1>
-        <p className="mx-auto max-w-[470px] py-12 text-lg leading-tight">
-          Check out our amazing features and experience the power of Vaultflow
-          for yourself.
-        </p>
+        <FadeOnScroll delay={0.4}>
+          <h1 className="relative z-10 mx-auto max-w-96 text-5xl leading-tight max-md:text-4xl">
+            Features that work for your future.
+          </h1>
+        </FadeOnScroll>
+        <FadeOnScroll delay={0.6}>
+          <p className="mx-auto max-w-[470px] py-12 text-lg leading-tight">
+            Check out our amazing features and experience the power of Vaultflow
+            for yourself.
+          </p>
+        </FadeOnScroll>
       </div>
       <CardsFeatures />
     </div>
