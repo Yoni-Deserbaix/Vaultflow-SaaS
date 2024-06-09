22d22ca9584987833217fd414b150314010415a7
unknown <yonideserbaix@gmail.com>
2024-06-09 15:20:08 +0200

    style(cardsFeatures): animation with framer motion

--- src/section/CardsFeatures.jsx
+++ src/section/CardsFeatures.jsx
@@ -1,4 +1,5 @@
 import { cardStyle } from "../components/cardGradient";
+import FadeOnScroll from "../components/FadeOnScroll";
 import data from "../data";
 
 export default function CardsFeatures() {
@@ -6,7 +7,7 @@ export default function CardsFeatures() {
     <div className="mx-auto max-w-screen-xl p-4">
       <div className="grid grid-cols-2 gap-6 max-[1024px]:grid-cols-1">
         {data.cardsData.slice(0, 2).map((card, index) => (
-          <div key={index} className="flex">
+          <FadeOnScroll delay={0.8} key={index} className="flex">
             <div
               className="flex h-full w-full flex-col justify-between rounded-3xl p-14"
               style={cardStyle}
@@ -27,41 +28,43 @@ export default function CardsFeatures() {
                 {card.viewLink}
               </p>
             </div>
-          </div>
+          </FadeOnScroll>
         ))}
       </div>
-      <div
-        className="mt-20 flex h-full w-full flex-row justify-between rounded-3xl p-14 max-lg:mt-6 max-md:flex-col"
-        style={cardStyle}
-      >
-        <div className="flex w-1/2 pr-4 max-lg:w-1/2 max-md:w-full">
-          <div>
+      <FadeOnScroll delay={0.4}>
+        <div
+          className="mt-20 flex h-full w-full flex-row justify-between rounded-3xl p-14 max-lg:mt-6 max-md:flex-col"
+          style={cardStyle}
+        >
+          <div className="flex w-1/2 pr-4 max-lg:w-1/2 max-md:w-full">
+            <div>
+              <img
+                src={data.cardsData[2].src}
+                alt="icon"
+                style={data.cardsData[2].shadow}
+                className="h-12 w-12 rounded-lg"
+              />
+              <h1 className="pb-3 pr-8 pt-4 text-[32px] text-[#ECECEC]">
+                {data.cardsData[2].title}
+              </h1>
+              <p className="w-full max-w-md text-[#ECECEC] max-sm:w-full">
+                {data.cardsData[2].caption}
+              </p>
+              <p className="mt-8 cursor-pointer text-white underline transition-all hover:text-gray-400">
+                {data.cardsData[2].viewLink}
+              </p>
+            </div>
+          </div>
+          <div className="flex w-1/2 items-center justify-center max-lg:mt-8 max-md:w-full">
             <img
-              src={data.cardsData[2].src}
-              alt="icon"
-              style={data.cardsData[2].shadow}
-              className="h-12 w-12 rounded-lg"
+              src={data.cardsData[2].imageCode}
+              alt="DigitalToken.js code"
+              style={cardStyle}
+              className="h-auto w-auto rounded-3xl"
             />
-            <h1 className="pb-3 pr-8 pt-4 text-[32px] text-[#ECECEC]">
-              {data.cardsData[2].title}
-            </h1>
-            <p className="w-full max-w-md text-[#ECECEC] max-sm:w-full">
-              {data.cardsData[2].caption}
-            </p>
-            <p className="mt-8 cursor-pointer text-white underline transition-all hover:text-gray-400">
-              {data.cardsData[2].viewLink}
-            </p>
           </div>
         </div>
-        <div className="flex w-1/2 items-center justify-center max-lg:mt-8 max-md:w-full">
-          <img
-            src={data.cardsData[2].imageCode}
-            alt="DigitalToken.js code"
-            style={cardStyle}
-            className="h-auto w-auto rounded-3xl"
-          />
-        </div>
-      </div>
+      </FadeOnScroll>
     </div>
   );
 }
--- src/section/Features.jsx
+++ src/section/Features.jsx
@@ -10,12 +10,12 @@ export default function Features() {
           alt=""
           className="absolute -top-16 left-0 right-0 m-auto w-[470px] transform"
         />
-        <FadeOnScroll delay={0.4}>
+        <FadeOnScroll delay={0.6}>
           <h1 className="relative z-10 mx-auto max-w-96 text-5xl leading-tight max-md:text-4xl">
             Features that work for your future.
           </h1>
         </FadeOnScroll>
-        <FadeOnScroll delay={0.6}>
+        <FadeOnScroll delay={0.8}>
           <p className="mx-auto max-w-[470px] py-12 text-lg leading-tight">
             Check out our amazing features and experience the power of Vaultflow
             for yourself.
