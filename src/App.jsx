e27be0de89612bb4164e71309632adafd4e0afaa
y2_dev <yonideserbaix@gmail.com>
2024-07-04 22:22:57 +0200

    style(app): few updates

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,21 @@ import Partner from "./section/Partner";
 
 export default function App() {
   return (
-    <div>
-      <div className="relative">
-        <div className="absolute w-full bg-[url('../public/assets/background.png')] bg-top bg-repeat-x">
-          <Navbar />
-          <div className="mx-auto max-w-4xl">
-            <Hero />
-          </div>
-          <div className="relative z-10 mx-auto mt-12 max-w-4xl">
-            <HeroImage />
-          </div>
-          <div className="-mt-12 bg-[#0b0121]">
-            <Partner />
-            <Features />
-            <HeroNoImage />
-            <Footer />
-          </div>
-        </div>
+    <div className="bg-[#0b0121] bg-[url('../public/assets/background.png')] bg-top bg-repeat-x">
+      <div className="p-8">
+        <Navbar />
+      </div>
+      <div className="mx-auto max-w-4xl">
+        <Hero />
+      </div>
+      <div className="relative z-10 mx-auto mt-12 max-w-4xl">
+        <HeroImage />
+      </div>
+      <div className="-mt-12 bg-[#0b0121]">
+        <Partner />
+        <Features />
+        <HeroNoImage />
+        <Footer />
       </div>
     </div>
   );
--- src/section/Navbar.jsx
+++ src/section/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div>
       <FadeDown delay={0}>
-        <div className="m-8 flex items-center justify-between rounded-3xl bg-[#0B081C] bg-opacity-50 px-10 py-2.5 text-[#ECECEC] max-sm:mx-2 max-sm:px-5 max-sm:py-1.5">
+        <div className="flex items-center justify-between rounded-3xl bg-[#0B081C] bg-opacity-50 px-10 py-2.5 text-[#ECECEC] max-sm:mx-2 max-sm:px-5 max-sm:py-1.5">
           <div className="flex cursor-pointer gap-2 text-xl">
             <img
               src="../assets/Vaultflow.svg"
